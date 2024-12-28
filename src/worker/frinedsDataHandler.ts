67aeb242224e75eea4cb9f0066e91b5c922ce323
Vaibhav Verma <vaibhavverma7475@gmail.com>
2024-12-28 19:41:20 +0530

    get friends api fixed

--- src/worker/frinedsDataHandler.ts
+++ src/worker/frinedsDataHandler.ts
@@ -2,33 +2,49 @@ import { StudentModel } from '../models/User';
 
 class FriendDataHandler {
 
-  async getFriends(username: string): Promise<{ username:string }[]> {
+  async getFriends(student_id: string): Promise<{ student_id:string }[]> {
     try {
       const result = await StudentModel.aggregate([
-        { $match: { name:username } },
-        { $unwind: "$friends" },
+        {
+          $match: {
+            student_id,
+          }
+        },
         {
           $lookup: {
             from: 'students',
             localField: 'friends',
             foreignField: '_id',
-            as: 'friendDetails'
+            as: 'friends',
+            pipeline: [
+              {
+                $project: {
+                  _id: 0,
+                  name: 1,
+                  student_id: 1,
+                }
+              }
+            ]
           }
         },
         {
           $project: {
             _id: 0,
-            sid: { $arrayElemAt: ["$friendDetails.student_id", 0] }
+            student_id: 1,
+            name: 1,
+            semester: 1,
+            branch: 1,
+            friends: 1,
           }
         }
       ]);
       if (result.length === 0) {
-        throw new Error(`Student with student_id ${username} not found`);
+        throw new Error(`Student with student_id ${student_id} not found`);
       }
-      return result.map((friend) => ({ username: friend.username }));
+      return result.map((friend) => ({ student_id: friend.student_id }));
     } catch (error) {
       console.error('Error fetching friends:', error);
-      throw new Error(`Error fetching friends for student_id ${username}: ${(error as any).message}`);
+      throw new Error(`Error fetching friends for student_id ${student_id}: ${(error)}`);
     }
   }
 }
