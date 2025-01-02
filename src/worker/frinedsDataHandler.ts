b8d642e50f29078ad9c2f1bf88c289f3bce0a81d
Vaibhav Verma <vaibhavverma7475@gmail.com>
2025-01-02 23:22:22 +0530

    fixed pipeline

--- src/worker/frinedsDataHandler.ts
+++ src/worker/frinedsDataHandler.ts
@@ -2,66 +2,67 @@ import dbConnect from '../db/connectDb';
 import { StudentModel } from '../models/User';
 import { studentData } from '../testData/studentData';
 
-// class FriendDataHandler {
-//   async getFriends(student_id: string): Promise<{ student_id: string, name: string }[]> {
-//     await dbConnect();
-//     try {
-//       const result = await StudentModel.aggregate([
-//         {
-//           $match: {
-//             student_id: student_id,
-//           },
-//         },
-//         {
-//           $lookup: {
-//             from: "students",
-//             localField: "friends",
-//             foreignField: "_id",
-//             as: "friends",
-//             pipeline: [
-//               {
-//                 $project: {
-//                   _id: 0,
-//                   student_id: 1,
-//                   name: 1,
-//                 },
-//               },
-//             ],
-//           },
-//         },
-//         {
-//           $unwind: {
-//             path: "$friends",
-//             preserveNullAndEmptyArrays: true,
-//           },
-//         },
-//         {
-//           $replaceRoot: { newRoot: "$friends" },
-//         },
-//       ]);
-//         return result.length > 0 ? result : [];
-  
-//     } catch (error) {
-//       console.error("Error fetching friends:", error);
-//       throw new Error(`Error fetching friends for student_id ${student_id}: ${(error as Error).message}`);
-//     }
-//   }
-// }  
-
-// export default FriendDataHandler;
-
-  //TESTT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 class FriendDataHandler {
-  async getFriends(student_id: string) {
-    const student = studentData.find((s) => s.student_id === student_id);
+  async getFriends(student_id: string): Promise<{ student_id: string, name: string }[]> {
+    await dbConnect();
+    try {
+      const result = await StudentModel.aggregate([
+        {
+          $match: {
+            student_id: student_id,
+          },
+        },
+        {
+          $lookup: {
+            from: "students",
+            localField: "friends",
+            foreignField: "_id",
+            as: "friends",
+            pipeline: [
+              {
+                $project: {
+                  _id: 0,
+                  student_id: 1,
+                  name: 1,
+                },
+              },
+            ],
+          },
+        },
+        {
+          $project: {
+            friends: 1
+          }
+        }
+      ]);
 
-    if (student) {
-      console.log("friend found");
-      
-      return student.friends;
+      if (!result || result.length === 0) {
+        return []
+      }
+
+      return result[0].friends;
+  
+    } catch (error) {
+      console.error("Error fetching friends:", error);
+      throw new Error(`Error fetching friends for student_id ${student_id}: ${(error as Error).message}`);
     }
-    return [];
   }
 }
 
 export default FriendDataHandler;
+
+  //TESTT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// class FriendDataHandler {
+//   async getFriends(student_id: string) {
+//     const student = studentData.find((s) => s.student_id === student_id);
+//
+//     if (student) {
+//       console.log("friend found");
+//
+//       return student.friends;
+//     }
+//     return [];
+//   }
+// }
+//
+// export default FriendDataHandler;
